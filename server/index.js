d5d5ee644b1dd33bafa4f191b0ac73d6df19afee
Ivan Missana <ivanmissana@gmail.com>
2024-01-14 23:02:58 +0300

    updates

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
+
 
 
 app.listen(port, () => {
