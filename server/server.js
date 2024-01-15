b9a29d3cdf8a4e396c48d8e8291da70cc5a68c32
Ivan Missana <ivanmissana@gmail.com>
2024-01-15 21:19:14 +0300

    updates

--- server/server.js
+++ server/server.js
@@ -4,9 +4,22 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect('your_mongodb_connection_string', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Routes (add your chat-related routes here)
 
 
 app.listen(port, () => {
