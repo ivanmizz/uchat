763614a959a72f7aeac0924835dc772ad0c8452c
Ivan Missana <ivanmissana@gmail.com>
2024-01-25 16:51:36 +0300

    updates

--- server/server.js
+++ server/server.js
@@ -1,31 +1,40 @@
-
-const express = require('express');
-const dotenv = require("dotenv");
-const cors = require('cors');
-const connectDB = require('./config/db');
-const chatRoutes = require('./routes/chatRoutes');
+import express from "express";
+import dotenv from "dotenv";
 const app = express();
-const PORT = process.env.PORT || 5000;
+import mongoose from "mongoose";
+import { createServer } from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+const server = createServer(app);
+import authRoute from "./routes/auth.js";
+import chatRoute from "./routes/chat.js";
+import messageRoute from "./routes/message.js";
+import authenticateUser from "./middleware/auth.js";
 
 dotenv.config();
-// Middleware
+const PORT = process.env.PORT || 4000;
+
 app.use(cors());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Connect to MongoDB
-// connectDB();
-mongoose.connect(
-  process.env.MONGO_URI, {
+mongoose
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }
-).then(() => console.log("database connected")).catch((err)=>console.error(err));
+  })
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.error(err));
 
-// Routes
-app.use('/api/chat', chatRoutes);
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Hey!! This is a sign that the server is running");
 });
+
+app.use("/auth", authRoute);
+app.use("/chat", authenticateUser, chatRoute);
+app.use("/message", authenticateUser, messageRoute);
+
+
+server.listen(PORT, () => console.log("Server is running on port", PORT));
+    
+    
