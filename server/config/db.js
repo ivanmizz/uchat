43455c3dcf9e7b38619a9991b95fb4aeebd4029f
Ivan Missana <ivanmissana@gmail.com>
2024-02-05 16:32:08 +0300

    updates

--- server/config/db.js
+++ server/config/db.js
@@ -1,23 +1,23 @@
-// server/config/db.js
-const mongoose = require('mongoose');
-import dotenv from 'dotenv';
-dotenv.config();
+// // server/config/db.js
+// const mongoose = require('mongoose');
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-const mongoURI = process.env.MONGO_URI;
+// const mongoURI = process.env.MONGO_URI;
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(mongoURI , {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
-    console.log('MongoDB connected');
-  } catch (error) {
-    console.error('MongoDB connection failed:', error);
-    process.exit(1);
-  }
-};
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(mongoURI , {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false,
+//     });
+//     console.log('MongoDB connected');
+//   } catch (error) {
+//     console.error('MongoDB connection failed:', error);
+//     process.exit(1);
+//   }
+// };
 
-module.exports = connectDB;
+// module.exports = connectDB;
