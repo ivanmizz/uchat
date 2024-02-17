2706bdb66c73d92f3c207c116989308cd082d56d
Ivan Missana <ivanmissana@gmail.com>
2024-02-17 08:49:14 +0300

    updates

---
+++ client/src/pages/chatpage.js
@@ -0,0 +1,16 @@
+
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Register from "./components/register"; 
+import Chatpage from "./pages/chatpage";
+import Login from "./components/login"; 
+
+
+function App() {
+  return 
+};
+
+export default App;
